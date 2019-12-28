1bfd1fa73f481dbc4623b4352476ff242d1dcaf6
jwalinarora <jwalinarora@gmail.com>
2019-12-28 17:22:08 +0530

    session part 2

---
+++ app.js
@@ -0,0 +1,149 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session=require('express-session');
+var FileStore=require('session-file-store')(session);
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var dishRouter	= require('./routes/dishRouter');
+var leaderRouter= require('./routes/leaderRouter');
+var promoRouter	= require('./routes/promoRouter');
+
+const mongoose=require('mongoose'); 
+
+
+console.log("Helloooooo");
+const url='mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+connect.then((db)=>{
+	console.log('Connected to the server');
+},(err)=>{console.log(err);});
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('halloweeen'));
+app.use(session({
+	name: 'session-id',
+	secret: 'halloweeen',
+	saveUninitialized: false,
+	resave: false,
+	store: new FileStore()
+}));
+
+// function auth(req,res,next){
+// 	console.log(req.signedCookies);
+// 	if(!req.signedCookies.user){               //user hasn't been authorised yet. User property has to be setup in cookie.
+// 		var authHeader=req.headers.authorization;
+// 		if(!authHeader){
+// 			var error=new Error('You are not authenticated');
+// 			res.setHeader("WWW-Authenticate","Basic");
+// 			err.status=401;
+// 			return next(err);
+// 		}
+// 		var auth= new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+
+// 		var username=auth[0];
+// 		var password=auth[1];
+		
+// 		if(username==='admin'&& password==='password'){
+// 			res.cookie('user','admin',{signed:true})   //(name, value ,properties)
+// 			next();
+// 		}
+// 		else{
+// 			var err= new Error('You are not authenticated');
+// 			res.setHeader('WWW-Authenticate','Basic');
+// 			err.status=401;
+// 			return next(err);
+// 		}	
+// 	}
+// 	else{
+// 		if(req.signedCookies.user==='admin'){
+// 			next();
+// 		}
+// 		else{
+// 			var err= new Error('You are not authenticated');
+// 			//res.setHeader('WWW-Authenticate','Basic');
+// 			err.status=401;
+// 			return next(err);
+// 		}
+// 	}
+	
+// }
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+function auth(req,res,next){
+	console.log(req.session);
+	if(!req.session.user){               //user hasn't been authorised yet. User property has to be setup in cookie.
+		// var authHeader=req.headers.authorization;
+		// if(!authHeader){
+			var err=new Error('You are not authenticated');
+			res.setHeader("WWW-Authenticate","Basic");
+			err.status=401;
+			return next(err);
+		}
+		// var auth= new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+
+		// // var username=auth[0];
+		// // var password=auth[1];
+		
+		// if(req.session.user==='authenticated'){
+		// 	//res.cookie('user','admin',{signed:true})   //(name, value ,properties)
+		// 	req.session.user='admin';
+		// 	next();
+		// }
+		// else{
+		// 	var err= new Error('You are not authenticated');
+		// 	res.setHeader('WWW-Authenticate','Basic');
+		// 	err.status=401;
+		// 	return next(err);
+		// }	
+	
+	else{
+		if(req.session.user==='authenticated'){
+			next();
+		}
+		else{
+			var err= new Error('You are not authenticated');
+			//res.setHeader('WWW-Authenticate','Basic');
+			err.status=401;
+			return next(err);
+		}
+	}
+	
+}
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+
+const Dishes=require('./models/dishes');
+
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
+app.use('/dishes',dishRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/promoRouter.js
@@ -0,0 +1,91 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const Promotions=require('../models/promotions');
+const promoRouter=express.Router();
+promoRouter.use(bodyParser.json());
+promoRouter.route("/")
+// .all((req,res,next)=>{
+// 	res.status=200;
+// 	res.setHeader('Content-Type',"text/plain");
+// 	next();
+// })
+.get((req,res,next)=>{
+	//res.end("Will send all the promotions to you!");
+	Promotions.find({})
+	.then((promotions)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(promotions);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.put((req,res,next)=>{
+	res.status=403
+	res.end("Put operation not supported on /promotions");
+})
+.post((req,res,next)=>{
+	//res.end("Will add the promotion: "+req.body.name+' with details: '+req.body.description);
+	Promotions.create(req.body)
+	.then((promotion)=>{
+		console.log("promotion created",promotion);
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(promotion);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.delete((req,res,next)=>{
+	//res.end("Deleting all promotions!	");
+	Promotions.remove({})
+	.then((resp)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(resp);
+		},(err)=>next(err))
+	.catch((err)=>next(err));
+});
+promoRouter.route("/:promoId")
+.all((req,res,next)=>{
+	res.status=200;
+	res.setHeader('Content-Type',"text/plain");
+	next();
+})
+.get((req,res,next)=>{
+	//res.end("Will send all details of "+req.params.promoId);
+	Promotions.findById(req.params.promoId)
+	.then((promotions)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(promotions);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.put((req,res,next)=>{
+	//res.end("Updating the promotion: "+req.params.promoId+'\n'+'Will update promotion: '+req.body.name+' with details '+req.body.description);
+	Promotions.findByIdAndUpdate(req.params.promotionId,{
+		$set:req.body
+	},{new:true})
+.then((promotion)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(promotion);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.post((req,res,next)=>{
+	res.statusCode=403;
+	res.end("POST operation not supported on /promotions/ "+req.params.dishId);
+})
+.delete((req,res,next)=>{
+	//res.end("Deleting promotion!");
+	Promotions.findByIdAndRemove(req.params.promotionId)
+	.then((promotion)=>{
+		res.statusCode=200;
+		res.setHeader('Content-Type','application/json');
+		res.json(promotion);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+
+});
+module.exports=promoRouter;
